ce81f3ad5debc92deffcdbe117bea4857e1bea50
ugurtepe1 <146785950+ugurtepe1@users.noreply.github.com>
2023-12-07 22:28:24 +0000

    completed

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError
+    ctr = 2
+    while (len(list) < number_of_primes):
+        ctr2 = 2
+        isPrime = True
+        while (ctr2 < ctr):
+            if ctr % ctr2 == 0:
+                isPrime = False
+            ctr2 += 1
+
+        if isPrime:
+            list.append(ctr)
+        
+        ctr+=1
+
     return list
